686d9be9c4c19bd13bc79943dbaf1c8d993ec9d5
Bhoomika <hmbhoomika2001@gmail.com>
2023-02-09 21:03:21 +0530

    program section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import Feature from "./components/Feature";
 
 
 function App() {
   return (
     <div>
       <Navbar/>
+      <Header/>
+      <Feature/>
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div id="main">
+        <div className='header-heading'>
+            <h2>MAKE IT</h2>
+            <h1><span>SHAPE</span>WITH US</h1>
+            <p className='details'>Build Your Body And Fitness With Professional Touch</p>
+            
+        </div>
+    </div>
+  )
+}
+
+export default Header
